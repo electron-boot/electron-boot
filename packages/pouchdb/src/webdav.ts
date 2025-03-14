5bc3e2878fc040333a63fee92f41adf02376bd68
RuffianLiu <876771120@qq.com>
2025-03-15 05:10:15 +0800

    feat(pouchdb): 增加数据库导出功能
    - 新增 WebDav 客户端支持导出数据库到 WebDAV 服务器
    - 添加数据库导入导出相关配置和接口
    -优化 PouchdbService 类，增加导出和导入方法
    - 更新 CHANGELOG.md，记录新增功能

---
+++ packages/pouchdb/src/webdav.ts
@@ -0,0 +1,80 @@
+import { createClient, type WebDAVClient } from 'webdav'
+import type { dumpOptions } from './types'
+import MemoryStream from 'memorystream'
+
+/**
+ * WebDav 数据库迁移
+ */
+export class WebDav {
+  private client: WebDAVClient
+  constructor(private options: dumpOptions) {
+    this.client = createClient(options.url, {
+      username: options.username,
+      password: options.password
+    })
+  }
+  /**
+   * 判断是否连接成功
+   * @returns 是否连接成功
+   */
+  async isReady(): Promise<boolean> {
+    try {
+      return await this.client.exists('/')
+    } catch (_e) {
+      throw new Error('WebDAV 连接失败')
+    }
+  }
+  /**
+   * 导出数据库
+   * @param dbInstance 数据库实例
+   * @returns 是否备份成功
+   */
+  async export(dbInstance: PouchDB.Database): Promise<void> {
+    try {
+      const result = await this.client.exists(this.options.cloudPath)
+      if (!result) {
+        await this.client.createDirectory(this.options.cloudPath)
+      }
+    } catch (e) {
+      throw new Error('WebDav目录创建出错:' + e)
+    }
+    const ws = new MemoryStream()
+    dbInstance.dump(ws, {
+      filter: (doc: { _attachments: any }) => {
+        // attachment 文档导出有问题，
+        return !doc._attachments
+      }
+    })
+
+    return new Promise((resolve, reject) => {
+      ws.pipe(this.client.createWriteStream(this.options.cloudPath, {}, () => {}))
+        .on('finish', () => {
+          console.log('WebDav 备份完成')
+          resolve()
+        })
+        .on('error', err => {
+          reject(err)
+        })
+    })
+  }
+
+  /**
+   * 导入数据库
+   * @param dbInstance 数据库实例
+   * @returns 是否导入成功
+   */
+  async import(dbInstance: PouchDB.Database): Promise<void> {
+    try {
+      const result = await this.client.exists(this.options.cloudPath)
+      if (!result) {
+        throw new Error('WebDav目录不存在')
+      }
+      const str = await this.client.getFileContents(this.options.cloudPath, {
+        format: 'text'
+      })
+      await dbInstance.loadIt(str)
+    } catch (e) {
+      throw new Error('WebDav目录导入出错:' + e)
+    }
+  }
+}
